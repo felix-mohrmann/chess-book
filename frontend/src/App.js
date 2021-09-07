3694968ceadda920d5841b1c8150e70eeef4264b
felix-mohrmann <mohrmann.felix@gmail.com>
2021-09-07 14:47:34 +0200

    small adjustments

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Login from './pages/Login'
 import Profile from './pages/Profile'
+import Openings from './pages/Openings'
 
 export default function App() {
   return (
