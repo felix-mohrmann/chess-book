2a4de7517af0091ff0eea6ec3c954f564ee8920d
felix-mohrmann <mohrmann.felix@gmail.com>
2021-09-14 15:18:01 +0200

    Add PageStyl to Openings Page

--- frontend/src/pages/Openings.js
+++ frontend/src/pages/Openings.js
@@ -1,14 +1,21 @@
-import styled from 'styled-components/macro'
 import OpeningCard from '../components/OpeningCard'
+import Navbar from '../components/Navbar'
+import PageStyle from '../components/PageStyle'
+import styled from 'styled-components/macro'
 
 export default function Openings() {
   return (
-    <Wrapper>
-      <h1>White</h1>
-      <OpeningCard />
-      <h1>Black</h1>
-    </Wrapper>
+    <PageStyle>
+      <Navbar />
+      <Wrapper>
+        <h1>White</h1>
+        <OpeningCard />
+        <h1>Black</h1>
+      </Wrapper>
+    </PageStyle>
   )
 }
 
-const Wrapper = styled.div``
+const Wrapper = styled.div`
+  display: grid;
+`
