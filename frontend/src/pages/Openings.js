29d0f9e70c0059364305932f298d641b1784e8de
felix-mohrmann <mohrmann.felix@gmail.com>
2021-09-14 15:14:54 +0200

    Add OpeningCard component

---
+++ frontend/src/pages/Openings.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components/macro'
+import OpeningCard from '../components/OpeningCard'
+
+export default function Openings() {
+  return (
+    <Wrapper>
+      <h1>White</h1>
+      <OpeningCard />
+      <h1>Black</h1>
+    </Wrapper>
+  )
+}
+
+const Wrapper = styled.div``
