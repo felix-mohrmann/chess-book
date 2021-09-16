27331783b9b6c74073e8927c9b355e27822981a3
felix-mohrmann <mohrmann.felix@gmail.com>
2021-09-16 14:09:57 +0200

    create axios request

--- frontend/src/pages/Openings.js
+++ frontend/src/pages/Openings.js
@@ -1,15 +1,34 @@
-import OpeningCard from '../components/OpeningCard'
 import Navbar from '../components/Navbar'
 import PageStyle from '../components/PageStyle'
 import styled from 'styled-components/macro'
+import { useEffect, useState } from 'react'
+import { useAuth } from '../auth/AuthProvider'
+import { getBlackOpenings, getWhiteOpenings } from '../service/chess-book-api'
 
 export default function Openings() {
+  const { username } = useAuth()
+  const [whiteOpenings, setWhiteOpenings] = useState()
+  const [blackOpenings, setBlackOpenings] = useState()
+
+  useEffect(() => {
+    getWhiteOpenings(username)
+      .then(setWhiteOpenings)
+      .catch(error => console.error(error))
+    getBlackOpenings(username)
+      .then(setBlackOpenings)
+      .catch(error => console.error(error))
+  }, [])
+
+  if (whiteOpenings) {
+    console.log(whiteOpenings)
+  }
+
   return (
     <PageStyle>
       <Navbar />
       <Wrapper>
         <h1>White</h1>
-        <OpeningCard />
+        {whiteOpenings && <h3>{whiteOpenings[0].name}</h3>}
         <h1>Black</h1>
       </Wrapper>
     </PageStyle>
@@ -17,5 +36,6 @@ export default function Openings() {
 }
 
 const Wrapper = styled.div`
-  display: grid;
+  display: flex;
+  flex-wrap: wrap;
 `
--- frontend/src/service/chess-book-api.js
+++ frontend/src/service/chess-book-api.js
@@ -2,3 +2,13 @@ import axios from 'axios'
 
 export const getParams = () =>
   axios.get('/api/chess-book/auth/params').then(response => response.data)
+
+export const getWhiteOpenings = name =>
+  axios
+    .get(`/api/chess-book/openings/white/${name}`)
+    .then(response => response.data)
+
+export const getBlackOpenings = name =>
+  axios
+    .get(`/api/chess-book/openings/black/${name}`)
+    .then(response => response.data)
