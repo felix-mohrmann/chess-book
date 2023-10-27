1af068978d686f932f27eaed4733586ae2ce5b8d
Felix <mohrmann.felix@gmail.com>
2023-10-26 18:20:57 -0600

    Updated React components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,8 @@
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Login from './pages/Login'
 import Profile from './pages/Profile'
 import Openings from './pages/Openings'
-import LichessRedirect from './auth/LichessRedirect'
+import LichessNavigate from './auth/LichessNavigate'
 import AuthProvider from './auth/AuthProvider'
 import Opening from './pages/Opening'
 import Variations from './pages/Variations'
@@ -11,17 +11,17 @@ export default function App() {
   return (
     <AuthProvider>
       <Router>
-        <Switch>
-          <Route exact path="/" component={Login} />
-          <Route path="/oauth/lichess_redirect" component={LichessRedirect} />
-          <Route path="/profile" component={Profile} />
+        <Routes>
+          <Route exact path="/" element={<Login />} />
+          <Route path="/oauth/lichess_redirect" element={<LichessNavigate />} />
+          <Route path="/profile" element={<Profile />} />
           <Route
             path="/openings/:openingName/:orientation/:fen1/:fen2/:fen3/:fen4/:fen5/:fen6/:fen7/:fen8"
-            component={Opening}
+            element={Opening}
           />
-          <Route path="/variations" component={Variations} />
-          <Route path="/openings" component={Openings} />
-        </Switch>
+          <Route path="/variations" element={<Variations />} />
+          <Route path="/openings" element={<Openings />} />
+        </Routes>
       </Router>
     </AuthProvider>
   )
